fa39202c378a705c1b53fec6e4c1e53f7013825c
анастасия лихоманова <>
2026-04-02 12:57:16 +0700

    было исправлено название класса и написаны API маршруты

--- backend/schemas/status_schema.py
+++ backend/schemas/status_schema.py
@@ -1,7 +1,7 @@
 from apiflask import Schema
 from apiflask.fields import String, Integer
 
-class StatusSchema(Schema):
+class StatusOutSchema(Schema):
     status = String()
     message = String()
     service = String()
