dda1cc1f45e950b477d2c86bb3129d427687cc15
анастасия лихоманова <>
2026-04-08 12:56:47 +0700

    Был добавлен бекенд клиент в директории фронтенда

---
+++ frontend/clients/backend_client.py
@@ -0,0 +1,10 @@
+import httpx
+
+class BackendApiClient:
+    def __init__(self, base_url: str):
+        self.base_url = base_url
+
+    def get_status(self):
+        response = httpx.get(f"{self.base_url}/api/items")
+        response.raise_for_status()
+        return response.json()
