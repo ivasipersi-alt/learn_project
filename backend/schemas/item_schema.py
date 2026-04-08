9740f37a65f0e19db3c0a3686a0b202cf8257cb0
анастасия лихоманова <>
2026-04-08 14:33:20 +0700

    Были исправлены все ошибки

--- backend/schemas/item_schema.py
+++ backend/schemas/item_schema.py
@@ -4,6 +4,8 @@ from apiflask.fields import String, Integer, List, Nested
 class ItemSchema(Schema):
     id = Integer()
     name = String()
+    description = String()
+    price = Integer()
 
 class ItemOutSchema(Schema):
     items = List(Nested(ItemSchema))
--- backend/services/status_service.py
+++ backend/services/status_service.py
@@ -2,11 +2,12 @@ def get_status_data():
     return {
         "status": "ok",
         "message": "Backend работает",
+        "service": "backend-service",
         "items_count": 3
     }
 def get_items_data():
-    return {
-        {"id": 1, "name": "Товар 1"},
-        {"id": 2, "name": "Товар 2"},
-        {"id": 3, "name": "Товар 3"}
-    }
+    return [
+        {"id": 1, "name": "Человек белый", "description": "что-то", "price": "1010"},
+        {"id": 2, "name": "Человек черный", "description": "что-то", "price": "1010"},
+        {"id": 3, "name": "Человек узкоглазый", "description": "что-то", "price": "1010"}
+    ]
